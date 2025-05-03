30e1e97d22975aac640dc39698b778a32fed6a6f
Hagar Elbakry <hagarelbakry12@gmail.com>
2025-05-03 15:47:38 +0300

    add user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Hagar Elbakry',
+            'username' => 'hagarelbakry',
+            'email' => 'hagar@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('hagar123'),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        User::create([
+            'name' => 'Menna Baligh',
+            'username' => 'mennabaligh',
+            'email' => 'mennab@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('mennab123'),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        User::create([
+            'name' => 'Menna Hamada',
+            'username' => 'mennahamada',
+            'email' => 'mennah@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('mennah123'),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        User::factory(20)->create();
+    }
+}
--- resources/views/explore.blade.php
+++ resources/views/explore.blade.php
@@ -14,6 +14,6 @@
                 </div>
             </a>
         @endforeach
-        {{$user->links}}
+        {{$users->links()}}
     </div>
 </x-app-layout>
