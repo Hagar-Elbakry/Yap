bb1b3c191bba27e9b344c2a5bffbd5614978db5c
Hagar Elbakry <hagarelbakry12@gmail.com>
2025-10-22 02:00:35 +0300

    add message icon next to follow button for chat feature

--- resources/views/components/follow-button.blade.php
+++ resources/views/components/follow-button.blade.php
@@ -1,4 +1,9 @@
 @if(currentUser()->isNot($user))
+    <div class="flex item-center space-x-2">
+        <a href="" class=" p-2 ">
+            <i class="fa-solid fa-envelope text-gray-500 hover:text-gray-700"></i>
+        </a>
+    </div>
 <form action="/profile/{{$user->username}}/follow" method="post">
     @csrf
     <button type="submit" class="bg-purple-950 rounded-full shadow py-2 px-4 text-white text-xs">
