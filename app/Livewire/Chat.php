71810b04d95daf7a01d34925506cab7fb88b71b5
Hagar Elbakry <hagarelbakry12@gmail.com>
2025-10-28 18:36:46 +0300

    implement message sending functionality in chat component

--- app/Livewire/Chat.php
+++ app/Livewire/Chat.php
@@ -2,15 +2,23 @@
 
 namespace App\Livewire;
 
+use App\Models\Message;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class Chat extends Component
 {
     public $user;
+    public $message;
+    public $senderId;
+    public $receiverId;
+    public $messages;
     public function mount($user)
     {
         $this->user = $this->getUser($user->id);
+        $this->senderId = Auth::user()->id;
+        $this->receiverId = $user->id;
     }
     public function render()
     {
@@ -20,5 +28,19 @@ class Chat extends Component
     public function getUser($userId)
     {
         return User::find($userId);
+    }
+
+    public function sendMessage() {
+        $this->saveMessage();
+        $this->message = '';
+    }
+
+    public function saveMessage() {
+        return Message::query()->create([
+            'message' => $this->message,
+            'sender_id' => $this->senderId,
+            'receiver_id' => $this->receiverId,
+            'is_read' => false,
+        ]);
     }
 }
